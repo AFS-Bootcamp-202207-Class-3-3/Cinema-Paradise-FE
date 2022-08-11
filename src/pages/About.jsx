870c7bfb57e5ecb3513646b792ae2ba24a09afff
LaceyShen <lacey.shen@oocl.com>
2022-08-11 11:43:55 +0800

    feat: smile in about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import {QRCodeCanvas} from 'qrcode.react';
+import React from "react";
+import { QRCodeCanvas } from "qrcode.react";
+import { SmileOutlined } from "@ant-design/icons";
+import { Result } from "antd";
 
 const About = () => {
   return (
     <div>
       About Me
       <QRCodeCanvas value="https://reactjs.org/" />,
+      <Result
+        icon={<SmileOutlined />}
+        title="If you encounter problems, please contact us!"
+      />
     </div>
   );
 };
