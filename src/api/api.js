97490ea22af12062a219a68da9d507f2647fc18a
jimmy.wang <jimmy.wang@oocl.com>
2022-08-11 15:33:06 +0800

    fix: use herokuapp in api not mockapi

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://cinema-paradise-afs.herokuapp.com/",
-  baseURL: "https://62f1e81025d9e8a2e7d2a81a.mockapi.io/api/",
+  baseURL: "https://cinema-paradise-afs.herokuapp.com/",
+  // baseURL: "https://62f1e81025d9e8a2e7d2a81a.mockapi.io/api/",
 });
 
 export default api;
