650b5e8658d3fa81ed04e71dec5d1cc525de8ffd
Leo xiao <leo.xiao@oocl.com>
2022-08-11 11:12:51 +0800

    change to mockapi for test

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cinema-paradise-afs.herokuapp.com/",
+  baseURL: "https://62f1e81025d9e8a2e7d2a81a.mockapi.io/api/",
 });
 
 export default api;
