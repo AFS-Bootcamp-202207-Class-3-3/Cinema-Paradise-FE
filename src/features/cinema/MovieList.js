4f114eb5df716ed9d641ba3eb4d687b4cdc7d30e
LaceyShen <lacey.shen@oocl.com>
2022-08-11 17:22:42 +0800

    feat: add divider in movielist

--- src/features/cinema/MovieList.js
+++ src/features/cinema/MovieList.js
@@ -1,4 +1,4 @@
-import { BackTop, Card, Carousel, Col, Layout, Row } from "antd";
+import { BackTop, Card, Carousel, Col, Layout, Row, Divider } from "antd";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -9,62 +9,70 @@ const { Content } = Layout;
 const { Meta } = Card;
 
 const CARD_STYLE = {
-    background: '#FFFAFA',
-    margin: 'auto',
-    width: '100%',
-    height: 'auto',
+  background: "#FFFAFA",
+  margin: "auto",
+  width: "100%",
+  height: "auto",
 };
 const CAROUSEL_STYLE = {
-    height: '50vh',
-    background: '#000000',
+  height: "50vh",
+  background: "#000000",
 };
 const CAROUSEL_IMG_STYLE = {
-    height: '50vh',
-    margin: 'auto',
+  height: "50vh",
+  margin: "auto",
 };
 
 function MovieList() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const movieList = useSelector((state) => state.movieList);
 
-    const movieList = useSelector((state) => state.movieList);
+  useEffect(() => {
+    getMovies().then((response) => {
+      dispatch(getAllMovies(response.data));
+    });
+  }, [dispatch]);
 
-    useEffect(() => {
-        getMovies().then((response) => {
-            dispatch(getAllMovies(response.data));
-        });
-    }, [dispatch]);
-
-    return (
-        <Layout>
-            <Content>
-                <Carousel autoplay className="aaaaaaa" style={CAROUSEL_STYLE}>
-                    {movieList.filter((rotationMovie) => rotationMovie.rotationImageUrl !== "").map((movie) => (
-                        <Link to={{ pathname: `/movies/${movie.id}` }} key={movie.id}>
-                            <img alt="carouselpic" src={movie.rotationImageUrl} style={CAROUSEL_IMG_STYLE} />
-                        </Link>
-                    ))}
-                </Carousel>
-                <Row gutter={[16, 16]} justify="space-evenly">
-                    {movieList.map((movie) => (
-                        <Col className="gutter-row" span={3} key={movie.id}>
-                            <Link to={{ pathname: `/movies/${movie.id}` }}>
-                                <Card
-                                    hoverable
-                                    style={CARD_STYLE}
-                                    cover={
-                                        <img alt="pic" src={movie.imageUrl} />}
-                                >
-                                    <Meta title={movie.titleChinese} description={movie.titleEnglish} />
-                                </Card>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-                <BackTop />
-            </Content>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Content>
+        <Carousel autoplay className="aaaaaaa" style={CAROUSEL_STYLE}>
+          {movieList
+            .filter((rotationMovie) => rotationMovie.rotationImageUrl !== "")
+            .map((movie) => (
+              <Link to={{ pathname: `/movies/${movie.id}` }} key={movie.id}>
+                <img
+                  alt="carouselpic"
+                  src={movie.rotationImageUrl}
+                  style={CAROUSEL_IMG_STYLE}
+                />
+              </Link>
+            ))}
+        </Carousel>
+        <Divider />
+        <Row gutter={[16, 16]} justify="space-evenly">
+          {movieList.map((movie) => (
+            <Col className="gutter-row" span={3} key={movie.id}>
+              <Link to={{ pathname: `/movies/${movie.id}` }}>
+                <Card
+                  hoverable
+                  style={CARD_STYLE}
+                  cover={<img alt="pic" src={movie.imageUrl} />}
+                >
+                  <Meta
+                    title={movie.titleChinese}
+                    description={movie.titleEnglish}
+                  />
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+        <BackTop />
+      </Content>
+    </Layout>
+  );
 }
 
 export default MovieList;
