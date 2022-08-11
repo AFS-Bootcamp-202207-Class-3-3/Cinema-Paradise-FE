a866cd8ee7e4737c3dbe9ce49406856c0128982d
VictoryZhao <victory.zhao@oocl.com>
2022-08-11 15:58:27 +0800

    refactor: Steps style

--- src/features/cinema/Steps2.js
+++ src/features/cinema/Steps2.js
@@ -1,18 +1,20 @@
 import { Steps } from 'antd';
 import React from 'react';
+import { Col,Row} from 'antd';
 const { Step } = Steps;
 
 function Steps2(){
 
     return (
-        <>
-        <Steps current={1}>
-        <Step title="Finished" description="选择电影场次" />
-        <Step title="In Progress" description="选择座位" />
-        <Step title="Waiting" description="选择支付" />
-        </Steps>    
-        </>
-
+        <Row justify="center" align="top">
+            <Col span={10}>
+                <Steps current={1}>
+                    <Step title="Finished" description="选择电影场次" />
+                    <Step title="In Progress" description="选择座位" />
+                    <Step title="Waiting" description="选择支付" />
+                </Steps>    
+            </Col> 
+        </Row>
     )
 }
 
--- src/features/cinema/Steps3.js
+++ src/features/cinema/Steps3.js
@@ -1,18 +1,19 @@
 import { Steps } from 'antd';
 import React from 'react';
+import { Col,Row} from 'antd';
 const { Step } = Steps;
-
 function Steps3(){
 
     return (
-        <>
-        <Steps current={2}>
-        <Step title="Finished" description="选择电影场次" />
-        <Step title="Finished"  description="选择座位" />
-        <Step title="In Progress" description="选择支付" />
-        </Steps>    
-        </>
-
+        <Row justify="center" align="top">
+            <Col span={10}>
+                <Steps current={2}>
+                    <Step title="Finished" description="选择电影场次" />
+                    <Step title="Finished"  description="选择座位" />
+                    <Step title="In Progress" description="选择支付" />
+                </Steps>    
+            </Col> 
+        </Row>
     )
 }
 
